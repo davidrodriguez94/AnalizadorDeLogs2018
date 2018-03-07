c98438d5a9dabf51ef19ab4147f7bc2ddd76d1b5
davidrodriguez94 <rodriguezdiezdavid94@gmail.com>
2018-03-07 12:38:34 +0100

    Cuarto commit

--- Acceso.java
+++ Acceso.java
@@ -6,64 +6,61 @@ public class Acceso
     private int hora;
     private int minutos;
     private String paginaWeb;
-    private String ip;
+    private String direccionIP;
     private int codigo;
-
-    /**
-     * Acceso Constructor
-     *
-     * @param momentoDeAcceso A parameter
-     */
+    
+    
     public Acceso(String momentoDeAcceso)
     {
-        String[] partesDeMomentoAcceso = momentoDeAcceso.split(" ");
-        ip = partesDeMomentoAcceso[0];
+        String[] partesDeMomentoAcceso = momentoDeAcceso.split(" "); 
+        direccionIP = partesDeMomentoAcceso[0];
         ano = Integer.parseInt(partesDeMomentoAcceso[1].substring(1,partesDeMomentoAcceso[1].length()));
-        mes = Integer.parseInt(partesDeMomentoAcceso[2]);
-        dia = Integer.parseInt(partesDeMomentoAcceso[3]);
+        mes = Integer.parseInt(partesDeMomentoAcceso[2]);        
+        dia = Integer.parseInt(partesDeMomentoAcceso[3]);    
         hora = Integer.parseInt(partesDeMomentoAcceso[4]);
-        minutos = Integer.parseInt(partesDeMomentoAcceso[5].substring(0,partesDeMomentoAcceso[0].length()-1));
+        minutos = Integer.parseInt(partesDeMomentoAcceso[5].substring(0,partesDeMomentoAcceso[5].length() - 1));
         paginaWeb = partesDeMomentoAcceso[6];
-        codigo = Integer.parseInt(partesDeMomentoAcceso[7]);
+        codigo =  Integer.parseInt(partesDeMomentoAcceso[7]);
     }
-
+    
     public int getAno() 
     {
         return ano;
     }
-
+    
     public int getMes()
     {
         return mes;
     }
-
+    
     public int getDia()
     {
         return dia;
     }
-
+    
     public int getHora()
     {
         return hora;
     }
-
+    
     public int getMinutos()
     {
         return minutos;
     }
     
-    public String getIp()
+    public String getDireccionIp()
     {
-        return ip;
-    }
-
-    public String getPaginaWeb()
-    {
-        return paginaWeb;
+        return direccionIP;
     }
     
-    public int getCodigo()
+    public String getPaginaWeb()
+    {
+        return paginaWeb;    
+    }
+
+    public int getCodigo() 
     {
         return codigo;
-    }
+    }    
+  
 }
--- AnalizadorAccesosAServidor.java
+++ AnalizadorAccesosAServidor.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AnalizadorAccesosAServidor
 {
+    private static final int CODIGO_EXITOSO = 200;
     private ArrayList<Acceso> accesos;
     
     
@@ -21,7 +23,7 @@ public class AnalizadorAccesosAServidor
             Scanner sc = new Scanner(archivoALeer);
             while (sc.hasNextLine()) {
                 String lineaLeida = sc.nextLine();               
-                Acceso accesoActual = new Acceso(lineaLeida);               
+                Acceso accesoActual = new Acceso(lineaLeida);              
                 accesos.add(accesoActual);
             }
             sc.close();
@@ -76,6 +78,7 @@ public class AnalizadorAccesosAServidor
                 if (urlActual.equals(accesoBucleInterno.getPaginaWeb())){
                     contadorAccesosUrlActual++;
                 }
+
             }
             
             if(contadorAccesosUrlActual >= maxNumAccesosHastaAhora){
@@ -84,7 +87,7 @@ public class AnalizadorAccesosAServidor
             }
         }
         
-        if (webMasSolicitada == null) {
+        if(webMasSolicitada == null) {
             System.out.println("No hay datos de acceso.");
         }
         return webMasSolicitada;
@@ -92,8 +95,80 @@ public class AnalizadorAccesosAServidor
     
     public String clienteConMasAccesosExitosos()
     {
-        return "";
+        String ipClienteMasAccesosExitosos = null;
+        HashMap<String, Integer> mapa = new HashMap<>(); 
+        
+        for(Acceso acceso : accesos) {
+            if(acceso.getCodigo() == CODIGO_EXITOSO) {
+                String ipAccesoActual = acceso.getDireccionIp();
+                
+                Integer valorAsociadoALaKey = mapa.get(ipAccesoActual);
+                if(valorAsociadoALaKey == null){
+                    //La ip no estaba en el mapa aun: la metemos asociada a un 1
+                    mapa.put(ipAccesoActual,1);
+                }
+                else {
+                    //Ya estaba en el mapa: incrementamos el valor asociado de accesos
+                    mapa.put(ipAccesoActual,mapa.get(ipAccesoActual)+1);
+                }
+            }
+        }
+        
+        int maximoAccesosHastaElMomento = 0;
+        
+        
+        for (String ipActual : mapa.keySet()) {
+            if ((mapa.get(ipActual) > maximoAccesosHastaElMomento) || 
+            ((mapa.get(ipActual) == maximoAccesosHastaElMomento) && (esMasAltaLaPrimeraIp(ipActual, ipClienteMasAccesosExitosos)))) {
+                maximoAccesosHastaElMomento = mapa.get(ipActual);
+                ipClienteMasAccesosExitosos = ipActual;
+            }
+        } 
+        
+        return ipClienteMasAccesosExitosos;
     }
+    
+    
+    private boolean esMasAltaLaPrimeraIp(String ipPrimera, String ipSegunda) 
+    {
+        boolean esMasAltaLaPrimera = false;
+        
+        if (ipSegunda == null) {
+            esMasAltaLaPrimera = true;
+        }
+        else {
+        
+            String[] octetosIP1 = ipPrimera.split("\\.");
+            String[] octetosIP2 = ipSegunda.split("\\.");
+        
+    
+            int indiceOctetoActual = 0;
+            boolean yaTengoUnaConclusion = false;
+            while(indiceOctetoActual < 4 && !yaTengoUnaConclusion) {
+                
+                int octetoIP1Actual = Integer.parseInt(octetosIP1[indiceOctetoActual]);
+                int octetoIP2Actual = Integer.parseInt(octetosIP2[indiceOctetoActual]);
+                
+                if (octetoIP1Actual > octetoIP2Actual) {
+                    esMasAltaLaPrimera = true;
+                    yaTengoUnaConclusion = true;
+                }
+                else if (octetoIP1Actual < octetoIP2Actual) {
+                    yaTengoUnaConclusion = true;
+                }
+                
+                indiceOctetoActual += 1;
+            }
+        }
+        
+
+        return esMasAltaLaPrimera;
+    }
+    
+    
+    
+    
+    
 
 
 }
